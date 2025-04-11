95f7cbdcab70e1f49430cefa7a02c66e64e373da
sam-143f1g <sarthak190405@gmail.com>
2025-04-11 15:16:12 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" 
 let accountState;
 
 // accountId = 2 // not allowed
